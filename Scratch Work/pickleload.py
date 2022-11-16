c95c1c4e616aa468345f6f782b92efbe8a392a2d
Greg Gompers <ggompers28@gmail.com>
2022-11-16 03:01:16 -0500

    adding pickle practice

--- Scratch Work/pickleload.py
+++ Scratch Work/pickleload.py
@@ -2,6 +2,7 @@ import pickle
 print(f'pickle version: {pickle.format_version}')
 
 from picklesave import MyTestClass
+# import picklesave
 
 with open('test.pkl', 'rb') as file:
     loaded_pickle = pickle.load(file)
